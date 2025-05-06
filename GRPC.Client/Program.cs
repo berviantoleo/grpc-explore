49f98e144b41894c92b818151ecf70e20510305a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-05-06 10:08:21 +0700

    feat: add docker-compose (#67)
    * feat: add docker-compose
    
    * fix: fix permission

--- GRPC.Client/Program.cs
+++ GRPC.Client/Program.cs
@@ -3,10 +3,8 @@ using Grpc.Net.Client;
 using GRPC.Client;
 
 // The port number must match the port of the gRPC server.
-using var channel = GrpcChannel.ForAddress("http://localhost:5255");
+using var channel = GrpcChannel.ForAddress(Environment.GetEnvironmentVariable("SERVER_URL") ?? "http://localhost:5255");
 var client = new Greeter.GreeterClient(channel);
 var reply = await client.SayHelloAsync(
                   new HelloRequest { Name = "GreeterClient" });
 Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
